0d9ccaf921b5390e3ad0cc6d8c79ff55f318257d
WottatoParrior <it214127@hua.gr>
2018-05-26 15:25:41 +0300

    Added the files

---
+++ start.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(express.static('./'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+	host : '35.204.240.189 ',
+	user: 'root',
+	password: 'uWGZ9L9vGX4n',
+	database: 'test'
+});
+
+connection.connect();
+
+app.get('/',(req,res) => {
+	res.send("Hello try at /books");
+});
+
+
+app.get('/books',function(req,res){
+	var {keyword} = req.query;
+	connection.query('SELECT * FROM books WHERE title LIKE ?','%'+keyword+'%', function (error, results, fields) {
+		if (error) throw error;
+		console.log(results);
+		res.send(results);
+		
+	});
+});
+
+app.post('/books',function(req,res){
+	var { title, author, genre, price } = req.body;
+    connection.query('INSERT INTO books (author,title,genre,price) VALUES (?, ?, ?, ?)', [author, title, genre, parseFloat(price)],function(error,results,fields){
+		if(error){
+			throw error;
+			alert("Could not add book, check if price is number")
+		}
+		res.json("book added");
+		
+	});
+	
+});
+
+
+app.listen(3000);
